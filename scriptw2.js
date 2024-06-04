bcf5ec9ffd5b1c2da3538ec3efbbd82bf60bf3f3
Tengen777 <theo30m@gmail.com>
2024-06-04 18:42:30 +0200

    Update scriptw2.js

--- scriptw2.js
+++ scriptw2.js
@@ -1,6 +1,17 @@
 'use strict'; 
-// Oefening 5-6 //copy paste van scrip 3 voor deze oefening
+//Oefening 5-6 //copy paste van scrip 3 voor deze oefening
+//Herwerk oefening 6. Het moet mogelijk zijn om meerdere studenten aan te maken. We passen de volgende zaken aan:
+//DONE 1.We maken op onze html pagina een knop met de naam: Create Student
+//DONE 2.We voegen een eventlistener toe. We luisteren naar het event click. (helemaal in het begin, verkorte schrijfwijze)
+//3.Wanneer we klikken op de knop, vragen we alle data op met behulp van de prompts. Net zoals in de vorige versie van de oefening.
+    //Op het einde van de bevraging, maken we een nieuw studentenobject aan. Ditmaal gebruiken we een constructor functie. Het object slagen we op in een lijst van studenten. 
+    //We kunnen op de knop van Create student nogmaals klikken voor het process te herstarten. 
+    //We voegen een knop toe aan onze html pagina met de naam: Show students. Wanneer we hierop klikken printen we de huidige lijst met studenten af op de html pagina. 
+
 console.log('script 3 linked!');
+
+    document.getElementById(elementId: 'create').addEventListener('click', createStudent); //createStudent is de functie, moet uitgevourd worden wanner er geclickt wordt op 'create'
+
 
 let student = {
     name: 'Default name',
@@ -22,7 +33,15 @@ let student = {
 
         console.log(txt);
     }
+
 }
+
+function createStudent () {  // () = geen parameters 
+
+
+}
+
+
 let data = [];
 //Using an array, we are storing all the answers as new elements of that array.
 data.push(prompt('Name?'));
