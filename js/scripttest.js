e299dfe5569cf5c6aef2a85b0236b275b57ebf4b
Tengen777 <theo30m@gmail.com>
2024-06-07 19:18:07 +0200

    Update scripttest.js
    obj.methode1 moet in de window.onload anders gaat het niet werken

--- js/scripttest.js
+++ js/scripttest.js
@@ -1,4 +1,17 @@
 window.onload = function(){
         // window is een (script uitvoeren als de pagina geladen is)
 
+    let obj ={
+        prop1: "This is a test",
+        prop2: 5,
+        methode1(param){
+        return param;
+
+        }   
+    }
+
+    obj.methode1("Sasuke");
+
 }
+
+
