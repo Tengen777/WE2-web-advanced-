61c5df45c2ac46ac624c7bde2ec4276a8b9ee552
Tengen777 <theo30m@gmail.com>
2024-06-07 21:54:53 +0200

    Create oef2.js

---
+++ js/oef2.js
@@ -0,0 +1,22 @@
+let color = {
+    nR: 0,
+    nG: 0,
+    nB: 0,
+    
+    showColor: function() {
+        console.log(`rgb(${this.nR}, ${this.nG}, ${this.nB})`);
+    },
+    
+    setColor: function(r, g, b) {
+        this.nR = r;
+        this.nG = g;
+        this.nB = b;
+    }
+};
+
+color.setColor(12, 34, 56);
+color.showColor(); // 
+color.setColor(255, 124, 56);
+color.showColor(); 
+color.setColor(255, 0, 100);
+color.showColor(); 
