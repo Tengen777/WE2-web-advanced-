efeecbd7f9afd22729003eabc0baec75a326d4c7
Tengen777 <theo30m@gmail.com>
2024-06-07 21:41:04 +0200

    Update oef1.js
    setColor erbij

--- js/oef1.js
+++ js/oef1.js
@@ -6,3 +6,11 @@ let color = {
     showColor: function() {
         console.log(`rgb(${this.nR}, ${this.nG}, ${this.nB})`);
     }
+
+
+    setColor: function(r, g, b) {
+        this.nR = r;
+        this.nG = g;
+        this.nB = b;
+    }
+};
